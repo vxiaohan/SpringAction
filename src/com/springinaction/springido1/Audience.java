1e486c028ef6f0de1d1a109c51c0cf3b03d782a5
vxiaohan <vxiaohan@outlook.com>
2016-04-09 19:59:17 +0800

    将AOP改为注解形式

--- src/com/springinaction/springido1/Audience.java
+++ src/com/springinaction/springido1/Audience.java
@@ -1,22 +1,38 @@
 package com.springinaction.springido1;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-
+@Aspect
 @Component
 public class Audience {
+	@Pointcut("execution(* com.springinaction.springido1.Performer.perform())")
+	public void performance(){
+		
+	}
+	@Before("performance()")
 	public void takeSeats(){
 		System.out.println("Take seats.");
 	}
+	@Before("performance()")
 	public void turnOffCellphone(){
 		System.out.println("Turn off cellphone.");
 	}
+	@AfterReturning("performance()")
 	public void applaud(){
 		System.out.println("Applaud..");
 	}
+	@AfterThrowing("performance()")
 	public void demandRefund(){
 		System.out.println("Refund");
 	}
+	
+	/*
 	public void watchPerformance(ProceedingJoinPoint joinpoint){
 		try{
 			takeSeats();
@@ -32,4 +48,5 @@ public class Audience {
 			System.out.println("Performace fail..");
 		}
 	}
+	*/
 }
